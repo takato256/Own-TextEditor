7b2bf6fbd977aa8d49a46707b94c0509285f2ee7
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-10-03 12:50:31 +0900

    complete step19

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+/*** includes ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
 
+/*** terminal ***/
 void die(const char *s) {
     perror(s);
     exit(1);
@@ -32,6 +35,7 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 int main() {
     enableRawMode();
 
